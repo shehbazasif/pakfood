f9314c3d4ab15e9678cc3998821f1a4cb2ee6f00
shehbazasif <m.shahbazasif512@gmail.com>
2024-09-13 13:48:38 +0500

    making grid

---
+++ web/use.js
@@ -0,0 +1,22 @@
+// function teacher(){
+//     let name="Shehbaz"
+//     return name;
+// }
+
+let CourseName=['Web Design', 'Graphic Design','ICS']
+
+
+const gF={
+    name1:"Sania",
+    fatherName:"Rana",
+}
+
+
+const student={
+    Name: "Abdul Rehman",
+    Course: CourseName,
+    girlFreind:gF,
+    RollNo: 1,
+    teacherName:teacher(),
+}
+console.log(student)
