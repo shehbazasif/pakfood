aef0ab6955b5171b4bc235f29767ee6385e8e883
shehbazasif <m.shahbazasif512@gmail.com>
2024-09-16 21:09:39 +0500

    css

--- web/use.js
+++ web/use.js
@@ -3,20 +3,29 @@
 //     return name;
 // }
 
-let CourseName=['Web Design', 'Graphic Design','ICS']
+// let CourseName=['Web Design', 'Graphic Design','ICS']
 
 
-const gF={
-    name1:"Sania",
-    fatherName:"Rana",
-}
+// const gF={
+//     name1:"Sania",
+//     fatherName:"Rana",
+// }
 
 
-const student={
-    Name: "Abdul Rehman",
-    Course: CourseName,
-    girlFreind:gF,
-    RollNo: 1,
-    teacherName:teacher(),
-}
-console.log(student)
+// const student={
+//     Name: "Abdul Rehman",
+//     Course: CourseName,
+//     girlFreind:gF,
+//     RollNo: 1,
+//     teacherName:teacher(),
+// }
+// console.log(student)
+
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    id: 5566,
+    };
+
+    const hersec=document.getElementsByClassName("hero-text-box")
